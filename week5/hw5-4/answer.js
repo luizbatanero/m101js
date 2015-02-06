c462f655bdbafe13f4214c78d4792a19691ac439
Luiz Batanero <luizbatanero@gmail.com>
2015-02-06 19:38:54 -0200

    hw5-4

--- week5/hw5-4/answer.js
+++ week5/hw5-4/answer.js
@@ -1,1 +1,13 @@
-//
+db.zips.aggregate([
+    { $match :
+        {
+            'city' : { $regex : /^\d/ }
+        }
+    },
+    { $group:
+        {
+            _id : null,
+            population: { $sum: "$pop" }
+        }
+    }
+])
